905437e25991f4a566d9e0996242866a0403c7ce
Marcemmanuel1 <108848425+Marcemmanuel1@users.noreply.github.com>
2025-07-04 18:32:18 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Inscription from "./components/Inscription";
 import Login from "./components/Login";
 import Page from "./components/Page";
-import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (

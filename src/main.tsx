68afc9c1ad11e2773cea9f77f283be91510a9b34
Marcemmanuel1 <broumarc175@gmail.com>
2025-07-04 14:31:21 +0000

    premier commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // Supprime StrictMode pour éviter le double rendu
+  <App />
 )
